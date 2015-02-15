3a32c1ef51bdc26c78c3322a31a5e8bafe37b93c
Jayden Patrick <jaydenpatrick1110@gmail.com>
2015-02-15 17:29:51 -0500

    Create Main.php

---
+++ src/SpecterCommands/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SpecterCommands\Specter;
+
+use specter\api\DummyPlayer
+use rankup\command\RankUpCommand
+
+
+public function onCommand (CommandSender $sender, Command $command, $label, array $args){
+		if($command->getName() == "SC") {
+$sender->getName()->sendMessage"SC] Commands:";
+$sender->getName()->sendMessage"SC] - /sc command";
+}
+  }
+
+  
